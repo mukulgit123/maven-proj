aeb5cce4347c70c6f0a416ba4555c0439f744260
mukul.garg <mukul.garg@ymail.com>
2019-02-20 06:57:46 +0000

    Added NoWhere.java

---
+++ src/main/java/hello/NoWhere.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class NoWhere {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
